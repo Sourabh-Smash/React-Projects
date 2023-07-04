97383a957c6a1325bec2d2154541a96f9f0c32d8
Sourabh <83724078+Sourabh-Smash@users.noreply.github.com>
2023-07-04 21:25:46 +0530

    Add files via upload

---
+++ project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body"
+
+const App=()=>{
+  return ( 
+    <>
+    <Navbar/>
+    <Body/>
+    </>
+  )
+}
+export default App;
---
+++ project-1/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="img" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    )
+}
+export default Navbar;
